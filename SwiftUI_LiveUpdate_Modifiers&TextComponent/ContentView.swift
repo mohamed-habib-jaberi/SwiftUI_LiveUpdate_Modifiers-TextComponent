e5efef43354d91ddcb5f9c6f146966ac6fd9f144
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-17 20:42:30 +0200

    Add modifiers to Text

--- SwiftUI_LiveUpdate_Modifiers&TextComponent/ContentView.swift
+++ SwiftUI_LiveUpdate_Modifiers&TextComponent/ContentView.swift
@@ -9,9 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+      var body: some View {
+      Text("Welcome to the Ray Wenderlich course on SwiftUI. In this course we are going to have an EPIC time working with all that SwiftUI has to offer. Remember, the answers are in the documentation :]")
+      .lineLimit(nil)
+          .minimumScaleFactor(0.5)
+          .multilineTextAlignment(.center)
+          .truncationMode(.middle)
+          .foregroundColor(Color.red)
+          .padding()
+          .background(Color.gray
+              .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/))
+          
+          .border(Color.black, width: 3)
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
