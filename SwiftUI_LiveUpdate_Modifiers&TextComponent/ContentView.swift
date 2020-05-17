62910943a7e8865790c005fff4535d5f8ad778bb
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-17 22:11:58 +0200

    Add Modifiers to change Font

--- SwiftUI_LiveUpdate_Modifiers&TextComponent/ContentView.swift
+++ SwiftUI_LiveUpdate_Modifiers&TextComponent/ContentView.swift
@@ -11,21 +11,31 @@ import SwiftUI
 struct ContentView: View {
       var body: some View {
       Text("Welcome to the Ray Wenderlich course on SwiftUI. In this course we are going to have an EPIC time working with all that SwiftUI has to offer. Remember, the answers are in the documentation :]")
-      .lineLimit(nil)
-          .minimumScaleFactor(0.5)
-          .multilineTextAlignment(.center)
-          .truncationMode(.middle)
-          .foregroundColor(Color.red)
-          .padding()
-          .background(Color.gray
-              .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/))
-          
-          .border(Color.black, width: 3)
+ //       .font(Font.system(.title, design: .rounded))
+//        .fontWeight(.bold)
+//        .underline()
+//        .italic()
+        .font(.custom("", size: 16))
+        .lineLimit(nil)
+        .minimumScaleFactor(0.5)
+        .multilineTextAlignment(.center)
+        .truncationMode(.middle)
+        .foregroundColor(.primary)
+        .padding()
+        .background(Color.gray)
+        
+        .border(Color.black, width: 3)
       }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+             ContentView()
+              .environment(\.colorScheme, .dark)
+            ContentView()
+                .environment(\.sizeCategory, .accessibilityExtraExtraLarge)
+              
+        }
     }
 }
